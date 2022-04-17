579f2151b0e4fed54c5fcce33e1a9d17c30f7d62
neauoire <aliceffekt@gmail.com>
2022-04-17 12:20:15 -0700

    Cleaning up uxn.js

--- src/uxn.js
+++ src/uxn.js
@@ -6,24 +6,19 @@ function Stack(u) {
 	this.p = 0
 	this.u = u
 
-	this.get8 = (id) => {
-		return this.mem[this.p-1-id]
-	}
-	
-	this.get16 = (id) => {
-		return this.mem[this.p-1-id] + (this.mem[this.p-2-id] << 8)
-	}
+	this.at8 = (i) => { return this.mem[i] }
+	this.at16 = (i) => { return (this.mem[i] << 8) + this.mem[i + 1] }
+	this.get8 = (i) => { return this.at8(this.p - 1 - i) }
+	this.get16 = (i) => { return this.at16(this.p - 2 - i) }
 
-	this.pop8 = () => {
-		if(this.p == 0x00) 
+	this.drop8 = () => {
+		if(this.p == 0x00)
 			return this.u.halt(1)
-		if(this.u.rk)
-			return this.mem[--this.pk]
-		return this.mem[--this.p]
+		return this.at8(this.u.rk ? --this.pk : --this.p)
 	}
 
-	this.pop16 = () => {
-		if(this.p < 0x02) 
+	this.drop16 = () => {
+		if(this.p < 0x02)
 			return this.u.halt(1)
 		if(this.u.rk)
 			return this.mem[--this.pk] + (this.mem[--this.pk] << 8)
@@ -31,13 +26,13 @@ function Stack(u) {
 	}
 
 	this.push8 = (val) => {
-		if(this.p == 0xff) 
+		if(this.p == 0xff)
 			return this.u.halt(2)
 		this.mem[this.p++] = val
 	}
 
 	this.push16 = (val) => {
-		if(this.p > 0xfe) 
+		if(this.p > 0xfe)
 			return this.u.halt(2)
 		this.mem[this.p++] = val >> 0x08
 		this.mem[this.p++] = val & 0xff
@@ -53,27 +48,19 @@ function Uxn (emu) {
 	this.rst = new Stack(this)
 	this.dev = new Uint8Array(0x100)
 
+	/* Control */
 	this.src = () => { return this.rr ? this.rst : this.wst }
 	this.dst = () => { return this.rr ? this.wst : this.rst }
-	this.pop8 = () => { return this.src().pop8() }
-	this.pop16 = () => { return this.src().pop16() }
-	this.pop = () => { return this.r2 ? this.pop16() : this.pop8() }
-	this.push8 = (x) => { this.src().push8(x) }
-	this.push16 = (x) => { this.src().push16(x) }
-	this.push = (val) => { if(this.r2) { this.push16(val) } else this.push8(val) }
 	this.peek = (addr) => { return this.r2 ? (this.ram[addr] << 8) + this.ram[addr + 1] : this.ram[addr] }
 	this.poke = (addr, val) => { if(this.r2) { this.ram[addr] = val >> 8; this.ram[addr + 1] = val; } else this.ram[addr] = val }
 	this.devr = (port) => { return this.r2 ? (this.emu.dei(port) << 8) + this.emu.dei(port+1) : this.emu.dei(port) }
 	this.devw = (port, val) => { if(this.r2){ this.emu.deo(port, val >> 8); this.emu.deo(port+1, val & 0xff) } else this.emu.deo(port, val) }
 	this.jump = (addr, pc) => { return this.r2 ? addr : pc + rel(addr); }
-
-	/* microcode */
-
-	this.get = (n) => { return this.r2 ? this.src().get16(n) : this.src().get8(n) }
-
-	this.pick = (n) => { this.push(this.get(n)) }
-
-	this.roll = (n) => { }
+	/* Stack Primitives */
+	this.push = (v) => { if(this.r2) this.src().push16(v); else this.src().push8(v) }
+	this.drop = (i) => { return this.r2 ? this.src().drop16(i) : this.src().drop8(i) }
+	this.roll = (i) => { this.push(this.drop(i)) }
+	this.pick = (i) => { this.push(this.r2 ? this.src().get16(i) : this.src().get8(i)) }
 
 	this.eval = (pc) => {
 		let a, b, c, instr
@@ -92,40 +79,40 @@ function Uxn (emu) {
 			switch(instr & 0x1f) {
 			/* Stack */
 			case 0x00: /* LIT */ this.push(this.peek(pc)); pc += !!this.r2 + 1; break;
-			case 0x01: /* INC */ this.push(this.pop() + 1); break;
-			case 0x02: /* POP */ this.pop(); break;
+			case 0x01: /* INC */ this.push(this.drop() + 1); break;
+			case 0x02: /* POP */ this.drop(0); break;
 			case 0x03: /* DUP */ this.pick(0); break;
-			case 0x04: /* NIP */ a = this.pop(); this.pop(); this.push(a); break;
-			case 0x05: /* SWP */ a = this.pop(); b = this.pop(); this.push(a); this.push(b); break;
+			case 0x04: /* NIP */ a = this.drop(); this.drop(); this.push(a); break;
+			case 0x05: /* SWP */ a = this.drop(); b = this.drop(); this.push(a); this.push(b); break;
 			case 0x06: /* OVR */ this.pick(1); break;
-			case 0x07: /* ROT */ a = this.pop(); b = this.pop(); c = this.pop(); this.push(b); this.push(a); this.push(c); break;
+			case 0x07: /* ROT */ a = this.drop(); b = this.drop(); c = this.drop(); this.push(b); this.push(a); this.push(c); break;
 			/* Logic */
-			case 0x08: /* EQU */ a = this.pop(); b = this.pop(); this.push8(b == a); break;
-			case 0x09: /* NEQ */ a = this.pop(); b = this.pop(); this.push8(b != a); break;
-			case 0x0a: /* GTH */ a = this.pop(); b = this.pop(); this.push8(b > a); break;
-			case 0x0b: /* LTH */ a = this.pop(); b = this.pop(); this.push8(b < a); break;
-			case 0x0c: /* JMP */ pc = this.jump(this.pop(), pc); break;
-			case 0x0d: /* JCN */ a = this.pop(); if(this.pop8()) pc = this.jump(a, pc); break;
-			case 0x0e: /* JSR */ this.dst().push16(pc); pc = this.jump(this.pop(), pc); break;
-			case 0x0f: /* STH */ this.dst().push16(this.pop()); break;
+			case 0x08: /* EQU */ a = this.drop(); b = this.drop(); this.src().push8(b == a); break;
+			case 0x09: /* NEQ */ a = this.drop(); b = this.drop(); this.src().push8(b != a); break;
+			case 0x0a: /* GTH */ a = this.drop(); b = this.drop(); this.src().push8(b > a); break;
+			case 0x0b: /* LTH */ a = this.drop(); b = this.drop(); this.src().push8(b < a); break;
+			case 0x0c: /* JMP */ pc = this.jump(this.drop(), pc); break;
+			case 0x0d: /* JCN */ a = this.drop(); if(this.src().drop8()) pc = this.jump(a, pc); break;
+			case 0x0e: /* JSR */ this.dst().push16(pc); pc = this.jump(this.drop(), pc); break;
+			case 0x0f: /* STH */ this.dst().push16(this.drop()); break;
 			/* Memory */
-			case 0x10: /* LDZ */ this.push(this.peek(this.pop8())); break;
-			case 0x11: /* STZ */ this.poke(this.pop8(), this.pop()); break;
-			case 0x12: /* LDR */ this.push(this.peek(pc + rel(this.pop8()))); break;
-			case 0x13: /* STR */ this.poke(pc + rel(this.pop8()), this.pop()); break;
-			case 0x14: /* LDA */ this.push(this.peek(this.pop16())); break;
-			case 0x15: /* STA */ this.poke(this.pop16(), this.pop()); break;
-			case 0x16: /* DEI */ this.push(this.devr(this.pop8())); break;
-			case 0x17: /* DEO */ this.devw(this.pop8(), this.pop()); break;
+			case 0x10: /* LDZ */ this.push(this.peek(this.src().drop8())); break;
+			case 0x11: /* STZ */ this.poke(this.src().drop8(), this.drop()); break;
+			case 0x12: /* LDR */ this.push(this.peek(pc + rel(this.src().drop8()))); break;
+			case 0x13: /* STR */ this.poke(pc + rel(this.src().drop8()), this.drop()); break;
+			case 0x14: /* LDA */ this.push(this.peek(this.src().drop16())); break;
+			case 0x15: /* STA */ this.poke(this.src().drop16(), this.drop()); break;
+			case 0x16: /* DEI */ this.push(this.devr(this.src().drop8())); break;
+			case 0x17: /* DEO */ this.devw(this.src().drop8(), this.drop()); break;
 			/* Arithmetic */
-			case 0x18: /* ADD */ a = this.pop(); b = this.pop(); this.push(b + a); break;
-			case 0x19: /* SUB */ a = this.pop(); b = this.pop(); this.push(b - a); break;
-			case 0x1a: /* MUL */ a = this.pop(); b = this.pop(); this.push(b * a); break;
-			case 0x1b: /* DIV */ a = this.pop(); b = this.pop(); this.push(b / a); break;
-			case 0x1c: /* AND */ a = this.pop(); b = this.pop(); this.push(b & a); break;
-			case 0x1d: /* ORA */ a = this.pop(); b = this.pop(); this.push(b | a); break;
-			case 0x1e: /* EOR */ a = this.pop(); b = this.pop(); this.push(b ^ a); break;
-			case 0x1f: /* SFT */ a = this.pop8(); b = this.pop(); this.push(b >> (a & 0x0f) << ((a & 0xf0) >> 4)); break;
+			case 0x18: /* ADD */ a = this.drop(); b = this.drop(); this.push(b + a); break;
+			case 0x19: /* SUB */ a = this.drop(); b = this.drop(); this.push(b - a); break;
+			case 0x1a: /* MUL */ a = this.drop(); b = this.drop(); this.push(b * a); break;
+			case 0x1b: /* DIV */ a = this.drop(); b = this.drop(); this.push(b / a); break;
+			case 0x1c: /* AND */ a = this.drop(); b = this.drop(); this.push(b & a); break;
+			case 0x1d: /* ORA */ a = this.drop(); b = this.drop(); this.push(b | a); break;
+			case 0x1e: /* EOR */ a = this.drop(); b = this.drop(); this.push(b ^ a); break;
+			case 0x1f: /* SFT */ a = this.src().drop8(); b = this.drop(); this.push(b >> (a & 0x0f) << ((a & 0xf0) >> 4)); break;
 			}
 		}
 	}
